cd7128b706e9717e625257cd08852e9cf0c4c521
The_Fireplace <the.f1repl4ce@gmail.com>
2017-08-14 22:39:54 -0500

    Make the void less forgiving

--- src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoid.java
+++ src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoid.java
@@ -1,9 +1,15 @@
 package net.blay09.mods.forgivingvoid;
 
+import net.minecraft.block.BlockPortal;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,7 +17,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = ForgivingVoid.MOD_ID, name = "Forgiving Void", acceptedMinecraftVersions = "[1.10.2]")
+import java.util.Random;
+
+@Mod(modid = ForgivingVoid.MOD_ID, name = "Less Forgiving Void", acceptedMinecraftVersions = "[1.10.2]")
 @Mod.EventBusSubscriber
 public class ForgivingVoid {
 
@@ -19,10 +27,37 @@ public class ForgivingVoid {
 
 	@SubscribeEvent
 	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if(event.side == Side.SERVER) {
+		if(event.side == Side.SERVER && event.phase == TickEvent.Phase.START) {
 			if(event.player.posY < 0) {
 				event.player.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 60, 3));
-				event.player.setPositionAndUpdate(event.player.posX, ModConfig.fallingHeight, event.player.posZ);
+				Random rand = new Random();
+				BlockPos spawnPos = new BlockPos(event.player.posX/8-ModConfig.baseDistanceOffset *2+rand.nextInt(ModConfig.baseDistanceOffset *4), rand.nextInt(100)+16, event.player.posZ/8-ModConfig.baseDistanceOffset *2+rand.nextInt(ModConfig.baseDistanceOffset *4));
+				event.player.setPortal(spawnPos);
+				if(event.player.changeDimension(-1) != null) {
+					int expandMult = 1;
+					while(event.player.worldObj.getBlockState(spawnPos).isNormalCube()){
+						spawnPos = new BlockPos(event.player.posX/8-ModConfig.baseDistanceOffset *2*expandMult+rand.nextInt(ModConfig.baseDistanceOffset *4*expandMult), rand.nextInt(100)+16, event.player.posZ/8-ModConfig.baseDistanceOffset *2*expandMult+rand.nextInt(ModConfig.baseDistanceOffset *4*expandMult));
+						event.player.setPortal(spawnPos);
+						expandMult++;
+					}
+					event.player.setPositionAndUpdate(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
+					//Even though the player doesn't spawn a portal by doing this, their sudden appearance should destroy any existing portal in the spot they appear.
+					for(int x=(int)event.player.posX-2;x<=event.player.posX+2;x++){
+						for(int y=(int)event.player.posY-3;y<=event.player.posY+3;y++){
+							for(int z=(int)event.player.posZ-2;z<=event.player.posZ+2;z++){
+								if(event.player.worldObj.getBlockState(new BlockPos(x, y, z)).getBlock() instanceof BlockPortal) {
+									event.player.worldObj.setBlockToAir(new BlockPos(x, y, z));
+									event.player.playSound(SoundEvents.BLOCK_GLASS_BREAK, 1.0f, 2.0f);
+								}
+							}
+						}
+					}
+
+					if(ModConfig.dropObsidian)
+						event.player.worldObj.spawnEntityInWorld(new EntityItem(event.player.worldObj, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), new ItemStack(Blocks.OBSIDIAN, 10)));
+				}else{
+					event.player.setPositionAndUpdate(event.player.posX-ModConfig.baseDistanceOffset *16+rand.nextInt(ModConfig.baseDistanceOffset *32), rand.nextInt(ModConfig.baseDistanceOffset)+ModConfig.baseDistanceOffset /2, event.player.posZ-ModConfig.baseDistanceOffset *16+rand.nextInt(ModConfig.baseDistanceOffset *32));
+				}
 				event.player.getEntityData().setBoolean("ForgivingVoidNoFallDamage", true);
 			}
 		}
@@ -47,14 +82,17 @@ public class ForgivingVoid {
 	@Config(modid = MOD_ID)
 	public static class ModConfig {
 		@Config.Comment("The amount of damage applied to the player when they land.")
-		@Config.RangeInt(min = 0, max = 20)
+		@Config.RangeInt(min = 0, max = 40)
 		public static int damageOnFall = 19;
 
-		@Config.Comment("Prevent death on void fall (limits damage to leave at least 0.5 hearts)")
-		public static boolean preventDeath = false;
+		@Config.Comment("Prevent death by fall damage when you fall to the nether (limits damage to leave at least 0.5 hearts)")
+		public static boolean preventDeath = true;
 
-		@Config.Comment("The height from which the player will be falling after falling through the void.")
-		@Config.RangeInt(min = 256, max = 4096)
-		public static int fallingHeight = 300;
+		@Config.Comment("Drop obsidian for the player when they appear in the Nether")
+		public static boolean dropObsidian = true;
+
+		@Config.Comment("The base offset from which the player will spawn from after falling through the void. This is not going to be the exact offset. Height is random.")
+		@Config.RangeInt(min = 128, max = 8192)
+		public static int baseDistanceOffset = 512;
 	}
 }
